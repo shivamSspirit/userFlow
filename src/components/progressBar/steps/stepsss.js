f535f61f80108be33cdd0047ce8f625a41cd375a
shivam <shivam.spirite3@outlook.com>
2022-06-11 10:28:44 +0530

    add some media queries

--- src/components/progressBar/steps/stepsss.js
+++ src/components/progressBar/steps/stepsss.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './stepsss.css'
+import { useGlobalFormValues } from '../../../fromDataContext'
 
 function Stepsss(props) {
     console.log(props)
     return (
-        <div className={`stepSection ${props.selected===props.index && "selected"}`}>
+        <div className={`stepSection ${(props.selected === props.index) && "selected"}`}>
             <div className={`circles`}>
                 <div onClick={() => props.updateSteps(props.index)} className={`cir`}>{props.index + 1}</div>
             </div>
