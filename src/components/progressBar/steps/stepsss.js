1f22e9567124f81601032cdde3eacfed8ab19f10
shivam <shivam.spirite3@outlook.com>
2022-06-11 15:56:06 +0530

    improve prograss bar style

--- src/components/progressBar/steps/stepsss.js
+++ src/components/progressBar/steps/stepsss.js
@@ -4,7 +4,7 @@ import './stepsss.css'
 function Stepsss(props) {
     console.log(props)
     return (
-        <div className={`stepSection ${(props.selected === props.index) && "selected"}`}>
+        <div className={`stepSection ${(props.index < props.selected + 1) && "selected"}`}>
             <div className={`circles`}>
                 <div onClick={() => props.updateSteps(props.index)} className={`cir`}>{props.index + 1}</div>
             </div>
